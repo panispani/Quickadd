b2086899f4489bc35b15b38636c8e3093123b1d6
panpan2 <p.panayiotou2@gmail.com>
2019-04-16 12:39:17 +0200

    Add script for adding events to calendar

---
+++ add_events.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If you modify these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+def main():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens
+    # Created automatically when the authorization flow completes for the first time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Read the input file events
+    with open("new_events.txt") as f:
+        events = f.read().splitlines()
+        # Add them to calendar
+        for event in events:
+            id = service.events().quickAdd(
+                calendarId='primary',
+                text=event).execute()
+            if not id:
+                print('"', event, '" not added.')
+
+    print("Events added!")
+
+
+if __name__ == '__main__':
+    main()
